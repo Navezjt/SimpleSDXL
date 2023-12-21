18e21eea46a2a8c0ec1f691cbefd04c7d6de4238
metercai <cai.mingjun@gmail.com>
2023-12-21 23:04:12 +0800

    fixed initiate process bug

--- enhanced/location.py
+++ enhanced/location.py
@@ -1,16 +1,16 @@
 import os
 import json
 import requests
-import maxminddb
+#import maxminddb
 from enhanced.models_hub_host import models_hub_host
 
 location = 'CN'
 try:
     public_ip = json.loads(requests.get(f'{models_hub_host}/ip/').text)['ip']
     geoip_path = os.path.abspath(f'./enhanced/geo.ip')
-    with maxminddb.open_database(geoip_path) as reader:
-        geoinfo = reader.get(public_ip)
-        if geoinfo:
-            location = geoinfo['country']['iso_code']
+#    with maxminddb.open_database(geoip_path) as reader:
+#        geoinfo = reader.get(public_ip)
+#        if geoinfo:
+#            location = geoinfo['country']['iso_code']
 except Exception as e:
     print(e)
