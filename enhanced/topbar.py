876dcabbc360bdc4ca317ed87d8f5cda1b9ca58b
metercai <cai.mingjun@gmail.com>
2024-05-31 10:03:54 +0800

    fixed preset switching bug

--- enhanced/topbar.py
+++ enhanced/topbar.py
@@ -342,7 +342,7 @@ preset_down_note_info = 'The preset package being loaded has model files that ne
 def check_absent_model(bar_button, state_params):
     #print(f'check_absent_model,state_params:{state_params}')
     state_params.update({'bar_button': bar_button})
-    return gr.update(visible=False), state_params
+    return state_params
 
 def down_absent_model(state_params):
     state_params.update({'bar_button': state_params["bar_button"].replace('\u2B07', '')})
@@ -354,7 +354,7 @@ def reset_params_for_preset(state_params):
     state_params.update({"__message": system_message})
     system_message = 'system message was displayed!'
     if '__preset' not in state_params.keys() or 'bar_button' not in state_params.keys() or state_params["__preset"]==state_params['bar_button']:
-        return [gr.update()] * 61 + [state_params]
+        return [gr.update()] * 60 + [state_params]
     if '\u2B07' in state_params["bar_button"]:
         gr.Info(preset_down_note_info)
     preset = state_params["bar_button"] if '\u2B07' not in state_params["bar_button"] else state_params["bar_button"].replace('\u2B07', '')
