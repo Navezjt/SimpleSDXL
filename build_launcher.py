56e52006b9c5129886adaf3d419a9d3facf0a448
metercai <cai.mingjun@gmail.com>
2024-06-07 21:57:51 +0800

    fixed

--- build_launcher.py
+++ build_launcher.py
@@ -17,7 +17,7 @@ def build_launcher():
     if not is_win32_standalone_build:
         return
 
-    branchs = {"SimpleSDXL_dev": "entry_with_update.py --dev", "SimpleSDXL_without_update": "launch.py", "SimpleSDXL_commit": "launch_with_commit.py"}
+    branchs = {"SimpleSDXL": "entry_with_update.py", "SimpleSDXL_without_update": "launch.py", "SimpleSDXL_commit": "launch_with_commit.py"}
 
     for (name, cmd) in branchs.items():
         win32_cmd_preset = win32_cmd.replace('{cmds}', f'{cmd}')
