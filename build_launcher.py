4c7fe482b2057bd8e0ec15c8ef48f5cc7b51dd19
metercai <cai.mingjun@gmail.com>
2024-07-02 15:06:36 +0800

    rebuild with_commit.bat and check and donwload SD1.5 model

--- build_launcher.py
+++ build_launcher.py
@@ -17,12 +17,12 @@ def build_launcher():
     if not is_win32_standalone_build:
         return
 
-    branchs = {"SimpleSDXL": "entry_with_update.py", "SimpleSDXL_dev": "entry_with_update.py --dev", "SimpleSDXL_without_update": "launch.py", "SimpleSDXL_commit": "launch_with_commit.py 56e520"}
+    branchs = {"SimpleSDXL": "entry_with_update.py", "SimpleSDXL_dev": "entry_with_update.py --dev", "SimpleSDXL_without_update": "launch.py", "SimpleSDXL_commit": "launch_with_commit.py 56e5200"}
 
     for (name, cmd) in branchs.items():
         win32_cmd_preset = win32_cmd.replace('{cmds}', f'{cmd}')
         bat_path = os.path.join(win32_root, f'run_{name}.bat')
-        if not os.path.exists(bat_path):
+        if not os.path.exists(bat_path) or bat_path=='run_SimpleSDXL_commit.bat':
             with open(bat_path, "w", encoding="utf-8") as f:
                 f.write(win32_cmd_preset)
     return
