415f70181a9bcfe8102f28d08c7bd32860177390
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2025-01-02 11:58:31 -0500

    Add files via upload

--- comfy/custom_nodes/ComfyUI-layerdiffuse/lib_layerdiffusion/models.py
+++ comfy/custom_nodes/ComfyUI-layerdiffuse/lib_layerdiffusion/models.py
@@ -7,19 +7,22 @@ from tqdm import tqdm
 from typing import Optional, Tuple
 from diffusers.configuration_utils import ConfigMixin, register_to_config
 from diffusers.models.modeling_utils import ModelMixin
-from diffusers.models.unets.unet_2d_blocks import UNetMidBlock2D, get_down_block, get_up_block
+import importlib.metadata
+from packaging.version import parse
 
+diffusers_version = importlib.metadata.version('diffusers')
 
-def check_diffusers_version():
-    import diffusers
-    from packaging.version import parse
-
-    assert parse(diffusers.__version__) >= parse(
-        "0.25.0"
-    ), "diffusers>=0.25.0 requirement not satisfied. Please install correct diffusers version."
-
+def check_diffusers_version(min_version="0.25.0"):
+    assert parse(diffusers_version) >= parse(
+        min_version
+    ), f"diffusers>={min_version} requirement not satisfied. Please install correct diffusers version."
 
 check_diffusers_version()
+
+if parse(diffusers_version) >= parse("0.29.0"):
+    from diffusers.models.unets.unet_2d_blocks import UNetMidBlock2D, get_down_block, get_up_block
+else:
+    from diffusers.models.unet_2d_blocks import UNetMidBlock2D, get_down_block, get_up_block
 
 
 def zero_module(module):
@@ -296,7 +299,16 @@ class TransparentVAEDecoder:
             result += [eps]
 
         result = torch.stack(result, dim=0)
-        median = torch.median(result, dim=0).values
+        if self.load_device == torch.device("mps"):
+            '''
+            In case that apple silicon devices would crash when calling torch.median() on tensors
+            in gpu vram with dimensions higher than 4, we move it to cpu, call torch.median()
+            and then move the result back to gpu.
+            '''
+            median = torch.median(result.cpu(), dim=0).values
+            median = median.to(device=self.load_device, dtype=self.dtype)
+        else:
+            median = torch.median(result, dim=0).values
         return median
 
     @torch.no_grad()
