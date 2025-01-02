d7782ad8a83655a376a2b083c01c5b9e37192fbf
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2025-01-02 12:06:00 -0500

    Add files via upload

---
+++ comfy/custom_nodes/rgthree-comfy/src_web/comfyui/utils_inputs_outputs.ts
@@ -0,0 +1,14 @@
+import type { LGraphNode } from "typings/litegraph.js";
+
+/** Removes all inputs from the end. */
+export function removeUnusedInputsFromEnd(node: LGraphNode, minNumber = 1, nameMatch?: RegExp) {
+  for (let i = node.inputs.length - 1; i >= minNumber; i--) {
+    if (!node.inputs[i]?.link) {
+      if (!nameMatch || nameMatch.test(node.inputs[i]!.name)) {
+        node.removeInput(i);
+      }
+      continue;
+    }
+    break;
+  }
+}
